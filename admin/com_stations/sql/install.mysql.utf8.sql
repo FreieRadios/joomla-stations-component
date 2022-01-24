d5279aa18766fb989c79f8d52bf1ac227910111b
michel <michael-liebler@janguo.de>
2022-01-24 09:08:09 +0100

    Mysql Error in install sql - refs #6

--- admin/com_stations/sql/install.mysql.utf8.sql
+++ admin/com_stations/sql/install.mysql.utf8.sql
@@ -1,84 +1,29 @@
-CREATE TABLE IF NOT EXISTS `#__stations_destinations`
-(
-    `id`
-    bigint
-    NOT
-    NULL
-    AUTO_INCREMENT,
-    `name`
-    varchar
-(
-    255
-) NOT NULL DEFAULT '',
-    `alias` varchar
-(
-    300
-) NOT NULL DEFAULT '',
-    `address` varchar
-(
-    200
-) NOT NULL,
-    `zipcode` varchar
-(
-    50
-) NOT NULL,
-    `town` varchar
-(
-    250
-) NOT NULL,
-    `www` varchar
-(
-    100
-) NOT NULL,
-    `livestream` varchar
-(
-    100
-) NOT NULL,
-    `catid` int
-(
-    11
-) NOT NULL DEFAULT 0,
-    `frn_id` int
-(
-    5
-) NOT NULL,
-    `mhz` varchar
-(
-    30
-) NOT NULL,
-    `lat` varchar
-(
-    200
-) NOT NULL,
-    `long` varchar
-(
-    200
-) NOT NULL,
-    `published` tinyint
-(
-    1
-) NOT NULL DEFAULT 0,
-
-    `assign_routeplanning` tinyint
-(
-    1
-) NOT NULL DEFAULT 1,
-    `ordering` int NOT NULL DEFAULT 0,
-    `created_by` int unsigned NOT NULL DEFAULT 0,
-    `access` int unsigned NOT NULL DEFAULT 1,
-    `country` int unsigned NOT NULL DEFAULT 0,
-    `language` varchar
-(
-    7
-) NOT NULL DEFAULT '*',
-    `created` datetime NOT NULL,
-    `modified` datetime NOT NULL,
-    `modified_by` int unsigned NOT NULL DEFAULT 0,
-    PRIMARY KEY
-(
-    `id`
+CREATE TABLE IF NOT EXISTS `#__stations_destinations` (
+                                                          `id` bigint NOT NULL AUTO_INCREMENT,
+                                                          `name` varchar NOT NULL DEFAULT '',
+                                                          `alias` varchar NOT NULL DEFAULT '',
+                                                          `address` varchar NOT NULL DEFAULT '',
+                                                          `zipcode` varchar NOT NULL DEFAULT '',
+                                                          `town` varchar NOT NULL DEFAULT '',
+                                                          `www` varchar NOT NULL DEFAULT '',
+                                                          `livestream` varchar NOT NULL DEFAULT '',
+                                                          `catid` int(11) NOT NULL DEFAULT 0,
+                                                          `frn_id` int(5) NOT NULL,
+                                                          `mhz` varchar(30) NOT NULL,
+                                                          `lat` varchar(200) NOT NULL,
+                                                          `long` varchar(200) NOT NULL,
+                                                          `published` tinyint(1) NOT NULL DEFAULT 0,
+                                                          `checked_out` int(11) DEFAULT NULL,
+                                                          `checked_out_time` datetime NOT NULL,
+                                                          `txt` text NOT NULL,
+                                                          `picture` varchar(255) NOT NULL DEFAULT '',
+                                                          `assign_routeplanning` tinyint(1) NOT NULL DEFAULT 1,
+                                                          `ordering` int(11) NOT NULL DEFAULT 0,
+                                                          `created_by` int(11) NOT NULL DEFAULT 0,
+                                                          `country` int(11) NOT NULL,
+                                                          `access` int(11) NOT NULL,
+                                                          `language` varchar(7) NOT NULL,
+                                                          `created` datetime NOT NULL,
+                                                          `modified` datetime NOT NULL,
+                                                          `modified_by` int(11) NOT NULL
 )
-    ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 DEFAULT COLLATE =utf8mb4_unicode_ci;
-
-
---{{inject: install_table}}
